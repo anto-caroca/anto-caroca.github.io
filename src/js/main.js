0a626ef16618c430fecc17e3b5233044a60dc3ca
antonella <antonella_caroca@hotmail.com>
2018-10-18 10:42:24 -0300

    agrego efectos con scrollReveal

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+
+        /* sobre mí*/ 
+        ScrollReveal().reveal('.about', { delay: 500 })
+		ScrollReveal().reveal('.bio', { delay: 1000 })
+        ScrollReveal().reveal('.box-2', { delay: 1300 })
+        ScrollReveal().reveal('.box-4', { delay: 1500 })
+        ScrollReveal().reveal('.box-3', { delay: 1800 })
+
+        /* proyectos*/
+        ScrollReveal().reveal('.projects', { delay: 500 })
+        ScrollReveal().reveal('.accordion', { delay: 1000 })
+        
+        /* contacto */ 
+        ScrollReveal().reveal('.contacto', { delay: 500 })
+        ScrollReveal().reveal('#linkedin', { delay: 1000 })
+        ScrollReveal().reveal('#github', { delay: 1200 })
+        ScrollReveal().reveal('#phone', { delay: 1500 })
+        ScrollReveal().reveal('#mail', { delay: 1800 })
+        ScrollReveal().reveal('#address', { delay: 2000 })
+
+        /*  tech skills */
+        ScrollReveal().reveal('.tech', { delay: 500 })
+        ScrollReveal().reveal('#div1', { delay: 1000 })
+        ScrollReveal().reveal('#div2', { delay: 1200 })
+        ScrollReveal().reveal('#div3', { delay: 1500 })
+        ScrollReveal().reveal('#div4', { delay: 1800 })
+        ScrollReveal().reveal('#div5', { delay: 2000 })
+        ScrollReveal().reveal('#div6', { delay: 2200 })
+        ScrollReveal().reveal('#div7', { delay: 2400 })
+        ScrollReveal().reveal('#div8', { delay: 2600 })
